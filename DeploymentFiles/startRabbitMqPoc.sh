363056c3e7fbf5036d08ed79f53c3bf14194966a
nic0michael <nicomichael2012@gmail.com>
2021-05-08 17:55:59 +0200

    Updated deployment files

--- DeploymentFiles/startRabbitMqPoc.sh
+++ DeploymentFiles/startRabbitMqPoc.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # startRabbitMqPoc.sh
 echo ---------------------------------------- >> start.log
-echo date >> start.log
 echo start.sh started >> start.log
 echo =================== >> start.log
 date >> start.log

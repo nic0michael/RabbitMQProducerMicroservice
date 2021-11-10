7c27f730656aaa4cb53131efa3fdb6fe004371e7
nic0michael <nicomichael2012@gmail.com>
2021-11-10 13:08:05 +0200

    getting newman shell script working

--- Postman/Newman/runbuffaloserver.sh
+++ Postman/Newman/runbuffaloserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-newman run /c/Users/NMichael/Projects/CuroProjects/RabbitMQProducerMicroservice/Postman/Newman/NicosQueueProducerCollectionBuffaloServer.json  --verbose > detailedResult.txt
+newman run NicosQueueProducerCollectionBuffaloServer.json  --verbose > detailedResult.txt
